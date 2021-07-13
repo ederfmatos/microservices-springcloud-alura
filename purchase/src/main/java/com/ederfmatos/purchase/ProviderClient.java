b1f2e5189e11abde228cafb374ca0fe6e4f647c7
Eder Matos <ederfmatos@gmail.com>
2021-07-13 09:38:02 -0300

    add request mapping to feign client

--- purchase/src/main/java/com/ederfmatos/purchase/ProviderClient.java
+++ purchase/src/main/java/com/ederfmatos/purchase/ProviderClient.java
@@ -3,12 +3,14 @@ package com.ederfmatos.purchase;
 import com.ederfmatos.purchase.model.Provider;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("provider")
+@RequestMapping("/providers")
 public interface ProviderClient {
 
-    @GetMapping("/providers/providers")
+    @GetMapping("/providers")
     Provider getProviderByState(@RequestParam("state") String state);
 
 }
